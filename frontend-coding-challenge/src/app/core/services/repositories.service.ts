cbc4065572f1ef7bc9f08f0efe2375965b5227a3
Mehdi AIT ARYANE <mehdi.aitaryane@gmail.com>
2020-09-27 20:57:26 +0100

    update service

--- frontend-coding-challenge/src/app/core/services/repositories.service.ts
+++ frontend-coding-challenge/src/app/core/services/repositories.service.ts
@@ -47,17 +47,4 @@ export class RepositoriesService {
         )
       );
   }
-
-  handleError(error): Observable<never> {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // client-side error
-      errorMessage = `Error: ${error.error.message}`;
-    } else {
-      // server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
-  }
 }
